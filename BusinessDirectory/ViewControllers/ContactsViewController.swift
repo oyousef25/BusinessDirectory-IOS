e05b53b2dad878603af87c34f627a6cd00ed9daf
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-10-20 13:41:55 -0400

    Added the tab bar navigation functionality

---
+++ BusinessDirectory/ViewControllers/ContactsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsViewController.swift
+//  BusinessDirectory
+//
+//  Created by Omar Yousef on 2021-10-20.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
