0b8ed03c05bd37f043cb32034fae5ab694edd3b0
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-10-20 17:37:13 -0400

    Created the view controllers classes that I'm going to need

---
+++ BusinessDirectory/ViewControllers/ContactsDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsDetailsViewController.swift
+//  BusinessDirectory
+//
+//  Created by Omar Yousef on 2021-10-20.
+//
+
+import UIKit
+
+class ContactsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
