23e5e13964f0ccf1c74f95c850daf857ea381206
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-10-23 13:04:14 -0400

    Finished setting up the cell for the contact tableview

--- BusinessDirectory/TableviewCells/ContactTableViewCell.swift
+++ BusinessDirectory/TableviewCells/ContactTableViewCell.swift
@@ -8,16 +8,69 @@
 import UIKit
 
 class ContactTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var businessImage: UIImageView!
+    
+    @IBOutlet weak var businessHead: UILabel!
+    
+    @IBOutlet weak var companyName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
+    
+    /*
+        This method is used to set the values for the UI elements with the object's returned data
+     */
+    func setUpCell(using business: Business){
+        businessHead.text = business.companyHead
+        companyName.text = business.businessName
+        
+        //set a default image in case there is no poster
+        businessImage.image = UIImage(named: "sample")
+        
+        //get the poster path string
+        guard let imagePath = business.companyLogo else { return }
+        
+        //build a url to fetch the album and load the image
+        if let url = buildImageUrl(for: imagePath){
+            print(url)
+            loadPoster(url: url, forCell: self)
+        }
+    }
+    
+    /*
+        This method will be building a valid image URL
+     */
+    func buildImageUrl(for path: String) -> URL? {
+        let imagePath = path
+        
+        guard let imageURL = URL(string: imagePath) else { return nil}
 
+        return imageURL
+    }
+    
+    /*
+        This method will load the image from the URL
+     */
+    func loadPoster(url: URL, forCell cell: ContactTableViewCell){
+        /*
+         Starting a new URL session to read the image from the url
+         */
+        let session = URLSession.shared
+        
+        let task = session.downloadTask(with: url){
+            url, response, error in
+            
+            if error == nil, let url = url, let data = try? Data(contentsOf: url), let companyLogo = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    cell.businessImage.image = companyLogo
+                }
+            }
+        }
+        task.resume()
+    }
 }
