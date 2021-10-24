822742b029f8873d93cd1171e694d387c6c1e7d6
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-10-24 15:45:00 -0400

    Created the core data subclasses

---
+++ BusinessDirectory/CoreDataSubclasses/ContactList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ContactList+CoreDataClass.swift
+//  BusinessDirectory
+//
+//  Created by Omar Yousef on 2021-10-24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ContactList)
+public class ContactList: NSManagedObject {
+
+}
---
+++ BusinessDirectory/CoreDataSubclasses/ContactList+CoreDataProperties.swift
@@ -0,0 +1,32 @@
+//
+//  ContactList+CoreDataProperties.swift
+//  BusinessDirectory
+//
+//  Created by Omar Yousef on 2021-10-24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ContactList {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ContactList> {
+        return NSFetchRequest<ContactList>(entityName: "ContactList")
+    }
+
+    @NSManaged public var companyHead: String?
+    @NSManaged public var contactId: Int32
+    @NSManaged public var contactLatitude: Double
+    @NSManaged public var contactLogo: String?
+    @NSManaged public var contactLongitude: Double
+    @NSManaged public var contactName: String?
+    @NSManaged public var contactNumber: String?
+    @NSManaged public var contactProducts: String?
+
+}
+
+extension ContactList : Identifiable {
+
+}
